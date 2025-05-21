7649ae70078033c0064a953dc2b90f3b87bb7e44
Sean Gerhardt <sean_gerhardt@hotmail.com>
2025-05-21 07:11:03 -0500

    Remove unused import

--- sqs/sqs.go
+++ sqs/sqs.go
@@ -5,7 +5,6 @@ import (
 	"log"
 
 	"github.com/aws/aws-sdk-go-v2/aws"
-	"github.com/aws/aws-sdk-go-v2/service/s3"
 	"github.com/aws/aws-sdk-go-v2/service/sqs"
 	"github.com/aws/aws-sdk-go-v2/service/sqs/types"
 )
